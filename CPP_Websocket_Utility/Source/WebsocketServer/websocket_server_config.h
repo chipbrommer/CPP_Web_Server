8717f2d4d2a9d66ce05813c39ad79fcd02373416
Chip Brommer <chipbrommer@gmail.com>
2023-04-23 13:40:58 -0500

    Change to mongoose version 7.9

--- CPP_Websocket_Utility/Source/WebsocketServer/websocket_server_config.h
+++ CPP_Websocket_Utility/Source/WebsocketServer/websocket_server_config.h
@@ -2,4 +2,4 @@
 			
 constexpr char DEFAULT_ADDRESS[]		= "http://127.0.0.1";
 constexpr char DEFAULT_WEB_ROOT[]		= "/index.html";
-constexpr uint16_t DEFAULT_PORT			= 5555;
+constexpr uint16_t DEFAULT_PORT			= 80;
