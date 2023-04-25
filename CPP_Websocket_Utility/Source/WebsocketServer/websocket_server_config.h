74f0618f9c541b6dfeeb81f737ecb9edd722b6f0
Chip Brommer <chipbrommer@gmail.com>
2023-04-24 21:41:29 -0500

    Add symlink for webpages into cmake.

--- CPP_Websocket_Utility/Source/WebsocketServer/websocket_server_config.h
+++ CPP_Websocket_Utility/Source/WebsocketServer/websocket_server_config.h
@@ -1,6 +1,6 @@
 #include <string>
 			
 constexpr char DEFAULT_ADDRESS[]		= "http://127.0.0.1";
-constexpr char DEFAULT_WEB_ROOT[]		= "/";
+constexpr char DEFAULT_WEB_ROOT[]		= "C:/website";
 constexpr uint16_t DEFAULT_PORT			= 80;
 static std::string web_root				= DEFAULT_WEB_ROOT;
